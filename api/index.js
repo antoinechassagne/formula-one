d6aaff28b15b39ee073805ee7cd033741d277fe2
Antoine Chassagne <antoine.chassagne@me.com>
2021-12-09 22:19:50 +0100

    Rename API port environment variable

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 (async () => {
   try {
-    await app.listen(process.env.APP_PORT);
+    await app.listen(process.env.PORT);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
