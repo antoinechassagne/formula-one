a2b077774e192c78dc3e733efaf215b19e53e217
Antoine Chassagne <antoine.chassagne@me.com>
2021-12-09 22:31:57 +0100

    Specify host on API

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 (async () => {
   try {
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT, '0.0.0.0');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
