02c26672c751ee357dd1f4eddc2db16bd80efe20
Antoine Chassagne <antoine.chassagne@me.com>
2022-01-08 00:42:45 +0100

    Activate SSL for production database

--- api/database/knexfile.js
+++ api/database/knexfile.js
@@ -24,7 +24,8 @@ module.exports = {
       host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME
+      database: process.env.DATABASE_NAME,
+      ssl: true
     },
     pool: {
       min: 0,
