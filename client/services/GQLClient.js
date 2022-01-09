a53b0c14ebc5c3914be20f88744d3c7d803733f5
Antoine Chassagne <antoine.chassagne@me.com>
2022-01-09 13:06:45 +0100

    Optimize GQLClient for SSG

--- client/services/GQLClient.js
+++ client/services/GQLClient.js
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const GQLClient = new ApolloClient({
   uri: process.env.API_URL,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  ssrMode: typeof window === "undefined"
 });
 
 export default GQLClient;
