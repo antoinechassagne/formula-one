eaa6c8c5c96ca6a3f93f6ebb14bba6bf3f51c56e
Antoine Chassagne <antoine.chassagne@me.com>
2022-05-26 23:39:24 +0200

    Add quote on homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <h1>Welcome</h1>
+      <p> Races are won at the track. Championships are won at the factory.</p>
+    </>
+  );
 }
