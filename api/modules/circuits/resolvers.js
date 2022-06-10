590d99904348062e4271788179b53f056d1ad25d
Antoine Chassagne <antoine.chassagne@me.com>
2022-06-10 08:16:22 +0200

    Add circuits API module

---
+++ api/modules/circuits/resolvers.js
@@ -0,0 +1,15 @@
+const { getCircuit, getCircuits } = require("./repository");
+
+async function circuit(_, { id }) {
+  const { data } = await getCircuit(id);
+  return data || null;
+}
+
+async function circuits(_, { query, skip, limit }) {
+  const { data } = await getCircuits(query, skip, limit);
+  return data || [];
+}
+
+module.exports = {
+  Query: { circuit, circuits }
+};
