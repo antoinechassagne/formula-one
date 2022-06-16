dcecdf27e6242b4afb4be9660ac303f7fa229736
Antoine Chassagne <antoine.chassagne@me.com>
2022-06-16 17:41:53 +0200

    Add team standings API module

---
+++ api/modules/team-standings/resolvers.js
@@ -0,0 +1,28 @@
+const { getTeamStanding, getTeamStandings } = require("./repository");
+const { getRace } = require("../races/repository");
+const { getTeam } = require("../teams/repository");
+
+async function teamStanding(_, { id }) {
+  const { data } = await getTeamStanding(id);
+  return data || null;
+}
+
+async function teamStandings(_, { query, skip, limit }) {
+  const { data } = await getTeamStandings(query, skip, limit);
+  return data || [];
+}
+
+async function race(teamStanding) {
+  const { data } = await getRace(teamStanding.raceId);
+  return data || null;
+}
+
+async function team(teamStanding) {
+  const { data } = await getTeam(teamStanding.teamId);
+  return data || null;
+}
+
+module.exports = {
+  Query: { teamStanding, teamStandings },
+  TeamResult: { race, team }
+};
