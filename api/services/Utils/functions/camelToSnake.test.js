d1ac0959931ba75d3ff17f4e1ec57c2857fdc359
Antoine Chassagne <antoine.chassagne@me.com>
2022-06-26 23:36:43 +0200

    Setup tests on client side

--- api/services/Utils/functions/camelToSnake.test.js
+++ api/services/Utils/functions/camelToSnake.test.js
@@ -1,15 +1,17 @@
-import { test, expect } from "vitest";
+import { describe, test, expect } from "vitest";
 
 const camelToSnake = require("./camelToSnake");
 
-test("Should return the value unchanged if it's either falsy or not a string", () => {
-  expect(camelToSnake(null)).toEqual(null);
-  expect(camelToSnake(undefined)).toEqual(undefined);
-  expect(camelToSnake([])).toEqual([]);
-});
+describe("camelToSnake function", () => {
+  test("Should return the value unchanged if it's either falsy or not a string", () => {
+    expect(camelToSnake(null)).toEqual(null);
+    expect(camelToSnake(undefined)).toEqual(undefined);
+    expect(camelToSnake([])).toEqual([]);
+  });
 
-test("Should transform from camel case to snake case", () => {
-  expect(camelToSnake("camelToSnake")).toEqual("camel_to_snake");
-  expect(camelToSnake("camel_to_snake")).toEqual("camel_to_snake");
-  expect(camelToSnake("camelTo_snake")).toEqual("camel_to_snake");
+  test("Should transform from camel case to snake case", () => {
+    expect(camelToSnake("camelToSnake")).toEqual("camel_to_snake");
+    expect(camelToSnake("camel_to_snake")).toEqual("camel_to_snake");
+    expect(camelToSnake("camelTo_snake")).toEqual("camel_to_snake");
+  });
 });
--- api/services/Utils/functions/snakeToCamel.test.js
+++ api/services/Utils/functions/snakeToCamel.test.js
@@ -1,14 +1,16 @@
-import { test, expect } from "vitest";
+import { describe, test, expect } from "vitest";
 
 const snakeToCamel = require("./snakeToCamel");
 
-test("Should return the value unchanged if it's either falsy or not a string", () => {
-  expect(snakeToCamel(null)).toEqual(null);
-  expect(snakeToCamel(undefined)).toEqual(undefined);
-  expect(snakeToCamel([])).toEqual([]);
-});
+describe("snakeToCamel function", () => {
+  test("Should return the value unchanged if it's either falsy or not a string", () => {
+    expect(snakeToCamel(null)).toEqual(null);
+    expect(snakeToCamel(undefined)).toEqual(undefined);
+    expect(snakeToCamel([])).toEqual([]);
+  });
 
-test("Should transform from snake case to camel case", () => {
-  expect(snakeToCamel("snake_to_camel")).toEqual("snakeToCamel");
-  expect(snakeToCamel("snake_toCamel")).toEqual("snakeToCamel");
+  test("Should transform from snake case to camel case", () => {
+    expect(snakeToCamel("snake_to_camel")).toEqual("snakeToCamel");
+    expect(snakeToCamel("snake_toCamel")).toEqual("snakeToCamel");
+  });
 });
